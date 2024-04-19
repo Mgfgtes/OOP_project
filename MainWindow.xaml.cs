b9ce8eecb9ab3cce8acbd7838e09beea0aee994f
Mgfgtes <117150053+Mgfgtes@users.noreply.github.com>
2024-04-19 17:03:04 +0200

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Accessibility;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TicTacToeGame game;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            // Získání jmen hráčů ze vstupních polí
+            string player1Name = Player1NameTextBox.Text; // X
+            string player2Name = Player2NameTextBox.Text; // O
+
+            if (string.IsNullOrWhiteSpace(player1Name) || string.IsNullOrWhiteSpace(player2Name) || player1Name == player2Name)
+            {
+                MessageBox.Show("Zadejte jména obou hráčů.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            
+
+            // Vytvoření instance herní třídy s jmény hráčů a velikostí hracího pole
+            int boardSize = int.Parse(((ComboBoxItem)BoardSizeComboBox.SelectedItem).Content.ToString().Split('x')[0]);
+            //game = new TicTacToeGame(player1Name, player2Name, boardSize);
+            InitializeGameBoard(boardSize);
+            game = new TicTacToeGame(player1Name,player2Name,boardSize);
+            Player1NameTextBox.IsEnabled = false;
+            Player2NameTextBox.IsEnabled = false;
+            startButton.IsEnabled = false;
+            BoardSizeComboBox.IsEnabled = false; 
+
+        }
+
+        private void NextRound_Click(object sender, RoutedEventArgs e)
+        {
+            Player1NameTextBox.IsEnabled = true;
+            Player2NameTextBox.IsEnabled = true;
+            startButton.IsEnabled = true;
+            BoardSizeComboBox.IsEnabled = true;
+            nextRound.Visibility = Visibility.Hidden;
+            loadPrevious.Visibility = Visibility.Visible;
+        }
+
+        private void LoadPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            loadPrevious.Visibility = Visibility.Hidden;
+            InitializeGameBoard(game.boardSize);
+            game.start();
+            Player1NameTextBox.IsEnabled = false;
+            Player2NameTextBox.IsEnabled = false;
+            startButton.IsEnabled = false;
+            BoardSizeComboBox.IsEnabled = false;
+        }
+
+        private void ShowLeaderboard_Click(object sender, RoutedEventArgs e) { }
+
+        private void Player1NameTextBox_TextChanged(object sender, TextChangedEventArgs e) { }
+
+        private void BoardSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
+        
+        public void HandleMove_Click(object sender, RoutedEventArgs e)
+        {
+        Button btn = (Button)sender;
+            if (!game.fin)
+            {
+                btn.IsEnabled = false;
+                if (String.Equals(game.currentPlayer, Player1NameTextBox.Text))
+                {
+                    btn.Content = "X";
+                }
+                else
+                {
+                    btn.Content = "O";
+                }
+                Tuple<int, int> position = (Tuple<int, int>)btn.Tag;
+
+                // Získání pozice tahu
+                int row = position.Item1;
+                int col = position.Item2;
+                game.add();
+                game.procCoords(row, col, btn.Content.ToString());
+                game.changeCurrentPlayer();
+                currentPlayerText.Content = "Pokračuje hráč: " + game.currentPlayer;
+            }
+            
+        }
+        private void InitializeGameBoard(int size)
+        {
+            // Vymazání starého obsahu herního pole
+            GameBoard.Items.Clear();
+            // Vytvoření nového herního pole
+            for (int row = 0; row < size; row++)
+            {
+                StackPanel rowPanel = new StackPanel { Orientation = Orientation.Horizontal };
+                for (int col = 0; col < size; col++)
+                {
+                    Button button = new Button
+                    {
+                        Tag = new Tuple<int, int>(row, col),
+                        Width = 50,
+                        Height = 50,
+                        Margin = new Thickness(5),
+                        Content = "",
+                        // Při kliknutí na tlačítko zavolá metodu HandleMove_Click pro zpracování tahu
+                        ClickMode = ClickMode.Press,
+                    };
+                    button.Click += HandleMove_Click;
+                    rowPanel.Children.Add(button);
+                }
+                GameBoard.Items.Add(rowPanel);
+            }
+        }
+
+        
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+        private string[,] board;
+        public string currentPlayer{ set; get; }
+        private string player1Name;
+        private string player2Name;
+        public int boardSize { set; get; }
+        private int counter;
+        public bool fin { get; set; }
+
+        public TicTacToeGame(string player1Name, string player2Name, int boardSize)
+        {
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
+            this.boardSize = boardSize;
+            this.fin = false;
+            start();
+        }
+
+        public void start()
+        {
+            board = new string[boardSize, boardSize];
+            Random rnd = new Random();
+            int startingplayer = rnd.Next(1, 3);
+            if (startingplayer == 1) {
+                currentPlayer = player1Name;
+            }
+            else
+                currentPlayer = player2Name;
+            ((MainWindow)System.Windows.Application.Current.MainWindow).currentPlayerText.Visibility = Visibility.Visible;
+            ((MainWindow)System.Windows.Application.Current.MainWindow).currentPlayerText.Content = "Začíná hráč: " + currentPlayer;
+
+
+        }
+
+        public void add()
+        {
+            counter++;
+            if(counter == boardSize*boardSize) {
+                MessageBox.Show("Remíza", "Piškvorky", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        public void procCoords(int x, int y, String playerSymbol)
+        
+        {
+            if(!fin) board[x, y] = playerSymbol;
+            if(CheckForWin())
+            {
+                fin = true;
+                MessageBox.Show("Vyhrál " + currentPlayer +", počet tahů: " + counter, "Výhra", MessageBoxButton.OK, MessageBoxImage.Information);
+                ((MainWindow)System.Windows.Application.Current.MainWindow).nextRound.Visibility = Visibility.Visible;
+            }
+            
+        }
+
+        private bool CheckForWin()
+        {
+            int cnt;
+            string previous;
+
+            //Vyhodnoceni viteztvi ve sloupcich
+            for (int c = 0; c < boardSize; c++)
+            {
+                cnt = 0;
+                previous = null;
+                for (int r = 0; r < boardSize; r++)
+                {
+                    if (board[r, c] != null && board[r, c] == previous) { cnt++; }
+                    else { cnt = 1; } // Pokud je aktuální prvek null nebo se liší od předchozího, resetujeme počítadlo na 1
+                    if (cnt == boardSize) { return true; }
+                    previous = board[r, c];
+                }
+            }
+
+            // Vyhodnocení vítězství v řádcích
+            for (int r = 0; r < boardSize; r++)
+            {
+                cnt = 0;
+                previous = null;
+                for (int c = 0; c < boardSize; c++)
+                {
+                    if (board[r, c] != null && board[r, c] == previous) { cnt++; }
+                    else { cnt = 1; } // Pokud je aktuální prvek null nebo se liší od předchozího, resetujeme počítadlo na 1
+                    if (cnt == boardSize) { return true; }
+                    previous = board[r, c];
+                }
+            }
+
+            // Vyhodnocení diagonální vítězství zleva dolů
+            cnt = 0;
+            previous = null;
+            for (int i = 0; i < boardSize; i++)
+            {
+                if (board[i, i] != null && board[i, i] == previous) { cnt++; }
+                else { cnt = 1; } // Pokud je aktuální prvek null nebo se liší od předchozího, resetujeme počítadlo na 1
+                if (cnt == boardSize) { return true; }
+                previous = board[i, i];
+            }
+
+            // Vyhodnocení diagonální vítězství zprava dolů
+            cnt = 0;
+            previous = null;
+            for (int i = 0; i < boardSize; i++)
+            {
+                if (board[i, boardSize - 1 - i] != null && board[i, boardSize - 1 - i] == previous) { cnt++; }
+                else { cnt = 1; } // Pokud je aktuální prvek null nebo se liší od předchozího, resetujeme počítadlo na 1
+                if (cnt == boardSize) { return true; }
+                previous = board[i, boardSize - 1 - i];
+            }
+
+            return false;
+        }
+
+        public String changeCurrentPlayer()
+        {
+        if (currentPlayer == player1Name)
+            {
+                currentPlayer = player2Name;
+            }
+        else
+            {
+                currentPlayer= player1Name;
+            }
+            return currentPlayer;
+        }
+    }
+}
